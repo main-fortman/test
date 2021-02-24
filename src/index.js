6aaa3e7645e0f02fbbe37632f268f7c1c14b5686
Oleg Baranov <main.fortman@gmail.com>
2021-02-24 12:30:16 +0300

    add test changes into  dev

--- src/index.js
+++ src/index.js
@@ -16,5 +16,5 @@ const heading = document.createElement('h1')
 heading.textContent = 'Как интересно!'
 
 // добавляем параграф и заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading, p)
+const root = document.querySelector('#root');
+root.append(heading, p);
